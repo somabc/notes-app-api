48ae73121bc2080a9cfe8fde496420952bd59a50
bcochran <b.cochrane@imperial.ac.uk>
2017-09-28 15:15:48 +0100

    test delete

--- .webpack/service/delete.js
+++ .webpack/service/delete.js
@@ -85,53 +85,44 @@ var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
 var main = exports.main = function () {
   var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(event, context, callback) {
-    var data, params, result;
+    var params, result;
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            data = JSON.parse(event.body);
             params = {
               TableName: "notes",
-              // 'Key' defines the partition key and sort key of the item to be updated
+              // 'Key' defines the partition key and sort key of the item to be removed
               // - 'userId': Identity Pool identity id of the authenticated user
               // - 'noteId': path parameter
               Key: {
                 userId: event.requestContext.identity.cognitoIdentityId,
                 noteId: event.pathParameters.id
-              },
-              // 'UpdateExpression' defines the attributes to be updated
-              // 'ExpressionAttributeValues' defines the value in the update expression
-              UpdateExpression: "SET content = :content, attachment = :attachment",
-              ExpressionAttributeValues: {
-                ":attachment": data.attachment ? data.attachment : null,
-                ":content": data.content ? data.content : null
-              },
-              ReturnValues: "ALL_NEW"
+              }
             };
-            _context.prev = 2;
-            _context.next = 5;
-            return dynamoDbLib.call("update", params);
+            _context.prev = 1;
+            _context.next = 4;
+            return dynamoDbLib.call("delete", params);
 
-          case 5:
+          case 4:
             result = _context.sent;
 
             callback(null, (0, _responseLib.success)({ status: true }));
-            _context.next = 12;
+            _context.next = 11;
             break;
 
-          case 9:
-            _context.prev = 9;
-            _context.t0 = _context["catch"](2);
+          case 8:
+            _context.prev = 8;
+            _context.t0 = _context["catch"](1);
 
             callback(null, (0, _responseLib.failure)({ status: false }));
 
-          case 12:
+          case 11:
           case "end":
             return _context.stop();
         }
       }
-    }, _callee, this, [[2, 9]]);
+    }, _callee, this, [[1, 8]]);
   }));
 
   return function main(_x, _x2, _x3) {
